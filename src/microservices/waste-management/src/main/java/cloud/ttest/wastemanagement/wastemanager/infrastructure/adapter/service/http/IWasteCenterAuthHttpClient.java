f3118f4bd0211ada193d394e42626f3c8f0f31ec
David Alejandro <david.rivas@externos.seresco.es>
2024-04-16 09:47:42 -0400

    fix: fixing autowired dependency error on IWasteCenterAuthHttpClient.java dependency injection

--- src/microservices/waste-management/src/main/java/cloud/ttest/wastemanagement/wastemanager/infrastructure/adapter/service/http/IWasteCenterAuthHttpClient.java
+++ src/microservices/waste-management/src/main/java/cloud/ttest/wastemanagement/wastemanager/infrastructure/adapter/service/http/IWasteCenterAuthHttpClient.java
@@ -1,10 +1,7 @@
 package cloud.ttest.wastemanagement.wastemanager.infrastructure.adapter.service.http;
 
-import org.springframework.stereotype.Component;
-
 import java.util.List;
 
-@Component
 public interface IWasteCenterAuthHttpClient {
     List<String> authorizationNumbers(List<Long> idList);
 }
