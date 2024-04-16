58f209d6a92c75c48f9ed98b35e709360662a9d1
David Alejandro <david.rivas@externos.seresco.es>
2024-04-16 12:12:03 -0400

    fix: fixing gateway documentation config.

--- src/cloudservices/gateway/src/main/java/cloud/ttest/gateway/share/infrastructure/conf/SwaggerConfig.java
+++ src/cloudservices/gateway/src/main/java/cloud/ttest/gateway/share/infrastructure/conf/SwaggerConfig.java
@@ -1,69 +1,36 @@
 package cloud.ttest.gateway.share.infrastructure.conf;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Contact;
 import io.swagger.v3.oas.models.info.Info;
 import io.swagger.v3.oas.models.info.License;
-import org.springdoc.core.models.GroupedOpenApi;
-import org.springdoc.core.properties.SwaggerUiConfigParameters;
-import org.springdoc.core.properties.SwaggerUiConfigProperties;
-import org.springframework.cloud.gateway.route.RouteDefinition;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
+import io.swagger.v3.oas.models.servers.Server;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.Lazy;
 
-import org.springframework.web.reactive.config.ResourceHandlerRegistry;
-import org.springframework.web.reactive.config.WebFluxConfigurer;
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @OpenAPIDefinition
-public class SwaggerConfig implements WebFluxConfigurer {
-
-    @Bean
-    @Lazy(false)
-    public List<GroupedOpenApi> apis(RouteDefinitionLocator locator) {
-        List<GroupedOpenApi> groups = new ArrayList<>();
-        List<RouteDefinition> definitions = locator.getRouteDefinitions().collectList().block();
-        assert definitions != null;
-        definitions.stream().filter(routeDefinition -> routeDefinition.getId().matches(".*-service"))
-                .forEach(routeDefinition -> {
-                    String name = routeDefinition.getId().replaceAll("-service", "");
-                    GroupedOpenApi api = GroupedOpenApi.builder().pathsToMatch("/" + name + "/**").group(name)
-                            .build();
-                    groups.add(api);
-                });
-        return groups;
-    }
-
+public class SwaggerConfig {
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
                 .components(new Components())
+                .servers(Arrays.asList(
+                                new Server().url("/waste-management/v3/api-docs").description("Waste Management"),
+                                new Server().url("/waste-center-auth/v3/api-docs").description("Waste Center Auth")))
                 .info(new Info()
-                        .title("Agencia Virtual API")
-                        .description("Documentation Agencia Virtual API v1.0")
+                        .title("Waste Management Cloud API")
+                        .description("Documentation Waste Management Cloud API v1.0")
                         .termsOfService("terms")
                         .contact(new Contact().email("@identity.dev"))
                         .license(new License().name("GNU"))
                         .version("v1.0"));
-    }
-
-    @Bean
-    public SwaggerUiConfigParameters swaggerUiConfigParameters() {
-        SwaggerUiConfigProperties properties = new SwaggerUiConfigProperties();
-        return new SwaggerUiConfigParameters(properties);
-    }
-
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/webjars/**")
-                .addResourceLocations("classpath:/META-INF/resources/webjars/")
-                .resourceChain(false);
     }
 }
